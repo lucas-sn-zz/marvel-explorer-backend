33baf9d0dd7fb0aed5ebf5e597b02534253fc36e
Lucas Neris <lucas.silva.neris@gmail.com>
2021-05-11 23:56:27 -0300

    add sql models

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
+from db import db
+from dotenv import load_dotenv
 from flask import Flask
-
+from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
+
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+api = Api(app)
 
 
 jwt = JWTManager(app)
+
+
+### routes
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 
 if __name__ == '__main__':
     db.init_app(app)
