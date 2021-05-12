fc0bfbdb0d3edabf13363e48ba30bab1dfe8d91e
Lucas Neris <lucas.silva.neris@gmail.com>
2021-05-12 00:25:40 -0300

    add marvel comics route

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from resources.comic import  UserComics, MarvelComics,MarvelComicDetails
 from resources.user import UserRegister, UserLogin, UserLogout,UserProfile
-
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['JWT_BLOCKLIST_ENABLED'] = True  # enable blocklist feature
+app.config['JWT_BLOCKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blocklisting for access and refresh tokens
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 api = Api(app)
@@ -27,11 +26,10 @@ jwt = JWTManager(app)
 
 api.add_resource(UserLogin, '/login')
 api.add_resource(UserLogout, '/logout')
-
-api.add_resource(UserRegister, '/register')
 api.add_resource(UserProfile, '/user/my_profile')
-
-
+api.add_resource(UserRegister, '/register')
+api.add_resource(MarvelComicDetails, '/marvel/comics/<int:id_comic>') # Ex: /marvel/comics/1009515
+api.add_resource(MarvelComics, '/marvel/comics')
 ### routes
 
 @app.before_first_request
